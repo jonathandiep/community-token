b3d2e9164338e1c52bdcb9d59467a12027f87b24
blockchainy <therulerguy@gmail.com>
2017-10-14 23:26:53 -0400

    updated contract

--- contracts/TicketToken.sol
+++ contracts/TicketToken.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.15;
+pragma solidity 0.4.17;
 
 import './ERC20.sol';
 import './SafeMath.sol';
@@ -9,36 +9,46 @@ contract TicketToken is ERC20 {
     
     uint public constant _totalSupply = 0;
 
-    string public constant symbol = 'CNT';
-    string public constant name = 'Community Network Token';
-    uint8 public constant decimals = 18;
-    
-    // 1 ether = 500 CNT
-    uint256 public constant RATE = 500;
-    
+    string public symbol;
+    string public name;
+    uint256 public price;
+    uint8 public constant decimals = 0;
+
     address public owner;
 
-    mapping(address => uint256) balances;
+    mapping(address => uint256) tickets;
+    
     // who is giving permission => (who is given permission to spend funds => how much allowed to spend)
     mapping(address => mapping(address => uint256)) approved;
     
     // callback function, so that people can directly send money to contract address
     function () payable {
-        createTokens();
+        buyTickets();
     }
 
 
-    function TicketToken() {
+    function TicketToken(string _name, string _symbol, uint256 _price) {
         owner = msg.sender;
+        tickets[owner] = 100;
+        name = _name;
+        symbol = _symbol;
+        price = _price;
     }
     
-    function createTokens() payable {
+    function buyTickets() payable {
         require(msg.value > 0);
-        
-        uint256 tokens = msg.value.mul(RATE);
-        balances[msg.sender] = balances[msg.sender].add(tokens);
-        
+        if(msg.value < price) {
+            throw;
+        } else {
+            // msg.sender.send(msg.value - _price)
+            transferFromOwnerToSender(1, msg.sender);
+        }
         owner.transfer(msg.value);
+    }
+    
+    function transferFromOwnerToSender(uint _amountOfTickets, address _sender) {
+        tickets[owner] = tickets[owner].sub(_amountOfTickets);
+        tickets[_sender] = tickets[_sender].add(_amountOfTickets);
     }
 
     function totalSupply() constant returns (uint _totalSupply) {
@@ -46,17 +56,17 @@ contract TicketToken is ERC20 {
     }
 
     function balanceOf(address _owner) constant returns (uint balance) {
-        return balances[_owner];
+        return tickets[_owner];
     }
 
     function transfer(address _to, uint _value) returns (bool success) {
         // require will throw an exception if any conditions inside fail
         require(
-            balances[msg.sender] >= _value &&
+            tickets[msg.sender] >= _value &&
             _value > 0 
         );
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
+        tickets[msg.sender] = tickets[msg.sender].sub(_value);
+        tickets[_to] = tickets[_to].add(_value);
         Transfer(msg.sender, _to, _value);
         return true;
     }
@@ -64,11 +74,11 @@ contract TicketToken is ERC20 {
     function transferFrom(address _from, address _to, uint _value) returns (bool success) {
         require(
             approved[_from][msg.sender] >= _value &&
-            balances[msg.sender] > _value &&
+            tickets[msg.sender] > _value &&
             _value > 0
         );
-        balances[_from] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[msg.sender].add(_value);
+        tickets[_from] = tickets[msg.sender].sub(_value);
+        tickets[_to] = tickets[msg.sender].add(_value);
         // lower the total amount allowed to spend
         approved[_from][msg.sender] = approved[_from][msg.sender].sub(_value);
         Transfer(_from, _to, _value);
@@ -84,9 +94,4 @@ contract TicketToken is ERC20 {
     function allowance(address _owner, address _spender) constant returns (uint remaining) {
         return approved[_owner][_spender];
     }
-    
-    function buyToken(uint _value) {
-        //value is number of tickets        
-    }
-
 } 
